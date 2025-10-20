a8eefa0e2cf41d4c17e4b2454f2c302862fd644e
codifuse <codifuse@gmail.com>
2025-10-20 19:13:52 +0600

    ADDED NOTIFICATION for personal user

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import Category, Transaction
+
+# Расширяем стандартную админ-панель пользователя
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')  # Добавляем id и другие поля
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('id',)  # Сортировка по id
+
+# Перерегистрируем модель User с кастомной админ-панелью
+admin.site.unregister(User)  # Сначала удаляем стандартную регистрацию
+admin.site.register(User, UserAdmin)  # Регистрируем с новой настройкой
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,6 +24,8 @@ class TransactionAdmin(admin.ModelAdmin):
     list_display = ['user', 'category', 'type', 'amount', 'created_at']
     list_filter = ['type', 'created_at']
     search_fields = ['user__username', 'category__name']
+
+
 
 # Создаем default категории при запуске
 from django.apps import AppConfig
